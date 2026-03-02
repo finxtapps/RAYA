88491ae505aca828f044d3d9adae99e087fe36e3
Suprit Kumar <suprit.22scse1012364@galgotiasuniversity.edu.in>
2026-03-02 15:23:15 +0530

    fixed the image size for deployment

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{backgroundImage: `url(${assets.header_img})`}}>
         <div className='header-contents'>
             <h2>Order your favourite food here</h2>
             <p>Choose form a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dinning experience, one delicious meal at a time.</p>
